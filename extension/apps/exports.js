abf7034f7f60b2af5c7f93696c392a0c8b5258c8
grekand46 <kluo0113@inst.hcpss.org>
2025-05-05 10:09:47 -0400

    Student manager test

--- extension/apps/exports.js
+++ extension/apps/exports.js
@@ -2,10 +2,12 @@ import { Soundboard } from "./soundboard.js";
 import { Group } from "./group.js";
 import { Picker } from "./picker.js";
 import { Home } from "./home.js";
+import { Students } from "./students.js";
 
 export const Apps = [
     Soundboard,
     Picker,
     Group,
+    Students,
     Home
 ];
