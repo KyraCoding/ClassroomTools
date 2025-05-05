6c8645ebc100eeb9ee7755857d3a4e89c0e46584
Christian <cha.christian.s@gmail.com>
2025-05-05 19:56:24 -0400

    Consolidated soundboard into 2 buttons

--- extension/apps/soundboard.js
+++ extension/apps/soundboard.js
@@ -1,24 +1,28 @@
 import { UI } from "../ui.js";
 
 const audios = [
-    { name: "Group Boo", file: "group-boo.mp3" },
-    { name: "Group Hooray", file: "group-hooray.mp3" },
-    { name: "Lone Boo", file: "lone-boo.mp3" },
-    { name: "Lone Hooray", file: "lone-hooray.mp3" },
+    { name: "Hooray", file: "group-hooray.mp3", color: "green" },
+    { name: "Boo", file: "group-boo.mp3", color: "red" }
 ];
 
-function createAudioButton({ name, file }) {
+function createAudioButton({ name, file, color }) {
+    const bgColor = color === "green" ? "bg-green-100" : "bg-red-100"
+    const borderColor = color === "green" ? "border-green-700" : "border-red-700";
+    const hoverBorderColor = color === "green" ? "hover:border-green-500" : "hover:border-red-500";
+    const textColor = color === "green" ? "text-green-700" : "text-red-700";
+    const hoverTextColor = color === "green" ? "hover:text-green-500" : "hover:text-red-500"
     const elem = UI.tag("div")
         .cls("flex", "flex-col", "justify-center", 
-            "items-center", "border-2", "border-black-700", 
-            "hover:border-blue-700", "rounded-lg", 
-            "bg-gray-100", "text-sm", "font-semibold", 
-            "text-black-700", "hover:text-blue-700", 
-            "p-4", "min-w-[100px]", "cursor-pointer", "transition-all")
-        .sub(name);
+            "items-center", "border-2", borderColor, 
+            hoverBorderColor, "rounded-lg", 
+            bgColor, "text-lg", "font-bold", 
+            textColor, hoverTextColor, 
+            "p-8", "w-full", "h-full", "cursor-pointer", "transition-all")
+        .sub(name)
+    
     const audio = new Audio(`assets/audio/${file}`);
-    elem.on("click", () => {
-        audio.play();
+    elem.on("click",() => {
+        audio.play()
     });
     return elem;
 }
@@ -29,14 +33,15 @@ export const Soundboard = {
     load(_state) {
         const container = UI.tag("div")
             .id("sb-list")
-            .cls("grid", "grid-cols-3", "gap-4");
+            .cls("flex", "flex-col", "gap-4", "w-full", "h-full")
         
         for (const info of audios) {
             container.add(createAudioButton(info));
         }
-        
+
         return UI.tag("div")
-            .cls("flex", "w-full", "py-4", "justify-center")
-            .sub(container);
+            .cls("flex", "w-full", "h-full", "py-4", "justify-center")
+            .sub(container)
     }
-};
+
+}
