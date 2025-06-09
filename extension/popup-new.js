62a55387a94a6105d650ac9bc3d6170d67babc0b
grekand46 <kluo0113@inst.hcpss.org>
2025-06-09 10:27:44 -0400

    App title shrink

--- extension/popup-new.js
+++ extension/popup-new.js
@@ -1,85 +1,33 @@
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    let tabs = document.querySelectorAll('.tab');
-    let contents = document.querySelectorAll('.tab-content');
-
-    tabs.forEach(function (tab) {
-        tab.addEventListener('click', function (e) {
-            let targetId = tab.id.replace('Tab', 'Content');
-
-            // Hide all content divs
-            contents.forEach(function (content) {
-                content.classList.add('hidden');
-            });
-
-            // Remove active class from all tabs
-            tabs.forEach(function (tab) {
-                tab.classList.remove('text-blue-700', 'bg-white');
-                tab.classList.add('text-slate-900');
-            });
-
-            // Show the target content
-            document.getElementById(targetId).classList.remove('hidden');
-
-            // Add active class to the clicked tab
-            tab.classList.add('text-blue-700', 'bg-white');
-            tab.classList.remove('text-slate-900');
-        });
-    });
-});
-*/
-
 import { Apps } from "./apps/exports.js";
 import { Storage } from "./storage.js";
 import { UI } from "./ui.js";
-
-/* 
-<div class="flex flex-col h-full w-1/6">
-    <div class="h-1/4 bg-sky-700 border-gray-700 border-2">
-        <span class="h-1/2">
-            <img class="aspect-square object-scale-down" src="/assets/imgs/wheel.png" />
-            <p class="text-center">Student Picker</p>
-        </span>
-    </div>
-    <div class="h-1/4  rounded-none bg-sky-700 border-gray-700 border-2">
-        <span class="h-1/2">
-            <img class="aspect-square object-scale-down" src="/assets/imgs/group.png" />
-            <p class="text-center">Group Gen</p>
-        </span>
-    </div>
-    <div class="h-1/4 bg-sky-700 border-gray-700 border-2">
-        <span class="h-1/2">
-            <img class="aspect-square object-scale-down" src="/assets/imgs/sound.png" />
-            <p class="text-center">Soundboard</p>
-        </span>
-    </div>
-    <div class="h-1/4 bg-sky-700 border-gray-700 border-2">
-        <span class="h-1/2">
-            <img class="aspect-square object-scale-down" src="/assets/imgs/home.png" />
-            <p class="text-center">Home</p>
-        </span>
-    </div>
-</div>
-*/
 
 const APP_COUNT = Apps.length;
 console.log("[debug] num of apps:", APP_COUNT);
 let currentApp = -1;
 
+/**
+ * @param {string} title 
+ * @param {*} param1 
+ * @returns 
+ */
 function createTabButton(title, {type, icon}) {
+    const DEFAULT_TEXT_SIZE = 14;
+    const OVERFLOW_LEN = 13;
+    const textSize = Math.round(DEFAULT_TEXT_SIZE * Math.min(1, OVERFLOW_LEN / title.length));
     return UI.tag("div").clz(`ct-tab-button flex grow h-1/${APP_COUNT} bg-sky-700 border-gray-700 border-0 m-1 box-content justify-center items-center rounded-lg`).sub(
         UI.tag("span").cls("h-1/2", "flex", "flex-col", "justify-center", "items-center").sub(
             type === "svg" ?
-                icon
+                icon.clz("aspect-square w-1/2")
                 : UI.tag("img").clz("grow aspect-square object-scale-down w-1/2").attr("src", icon),
-            UI.tag("p").cls("text-center").sub(title)
+            UI.tag("p").cls("text-center", `text-[${textSize}px]`, "whitespace-nowrap").sub(title)
         )
     );
 }
 
 /** @type {UI[]} */
 const tabButtons = [];
-const tabPane = UI.tag("div").clz("ct-tab-pane flex flex-col h-full w-1/6 select-none");
+const tabPane = UI.tag("div").clz("ct-tab-pane flex flex-col h-full w-1/6 select-none sticky");
 for (const [i, app] of Apps.entries()) {
     let type, icon;
     if (app.svg !== undefined) {
